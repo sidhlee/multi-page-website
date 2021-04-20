746949bc1e5c69a131303ec91dc240b7e21e65cd
Sid Hayoun Lee <sidhlee@gmail.com>
2021-04-20 19:10:17 -0400

    feat: add style to  active link

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
 AOS.init();
 
-const toggler = document.querySelector('.menu-toggler');
-
-// Trying to find out why clicking on link doesn't toggle the checkbox
-// menuModal.addEventListener('click', (e) => {
-// 	console.log('click')
-// 	console.log(e.target)
-// 	console.log(toggler.checked)
-// })
-
 const links = document.querySelectorAll('.nav-links > li > a');
-links.forEach(function (link) {
-	link.addEventListener('click', handleLinkClick);
-});
+const toggler = document.querySelector('.menu-toggler');
+const url = new URL(window.location.href);
+// pathname doesn't contain hash
+const currentPath = url.pathname + url.hash;
+
+function matchPaths(a, b) {
+	return a.replace(/\//g, '') === b.replace('/', '');
+}
+
+// Add .active to all links that match the given path and remove it from other links
+function styleActiveLink(activeLinkHref) {
+	links.forEach((link) => {
+		const linkHref = link.attributes.href.value;
+		const isMatch = matchPaths(activeLinkHref, linkHref);
+
+		if (isMatch) {
+			link.classList.add('active');
+		} else {
+			link.classList.remove('active');
+		}
+	});
+}
 
 function handleLinkClick(e) {
 	// don't need to call e.preventDefault() because the browser will redirect AFTER running the block
 
-	// toggle .active for active link styling
-	toggleActiveClass(this);
-	// toggle checkbox value(checked)
-	toggler.click();
+	const clickedLinkPath = this.attributes.href.value;
+	styleActiveLink(clickedLinkPath);
+
+	// toggle checkbox value(checked) except when the user's on homepage and modal is not open.
+	// (just navigate to internal link without opening modal)
+	const isModalOpen = toggler.checked;
+	const isHomePage = currentPath === '/';
+	if (isHomePage && !isModalOpen) {
+		return;
+	} else {
+		toggler.click();
+	}
 }
 
-function toggleActiveClass(linkElem) {
-	links.forEach((link) => link.classList.remove('active'));
-	linkElem.classList.add('active');
-}
+// Add handlers to links
+links.forEach(function (link) {
+	link.addEventListener('click', handleLinkClick);
+});
+
+// activate links on page load
+styleActiveLink(currentPath);
