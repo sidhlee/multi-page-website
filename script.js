7b3bcaeb2654e787e27d8775bb212b2443b326dc
Sid Hayoun Lee <sidhlee@gmail.com>
2021-04-20 17:20:42 -0400

    feat: toggle active class on navlink click

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 AOS.init();
 
-const aboutLink = document.querySelector('.nav-modal > ul > li:nth-of-type(2)');
 const toggler = document.querySelector('.menu-toggler');
 
-aboutLink.addEventListener('click', uncheckToggler);
-
-function uncheckToggler() {
-	toggler.click();
-}
-
 // Trying to find out why clicking on link doesn't toggle the checkbox
-// const menuModal = document.querySelector('.menu-modal')
 // menuModal.addEventListener('click', (e) => {
 // 	console.log('click')
 // 	console.log(e.target)
 // 	console.log(toggler.checked)
 // })
+
+const links = document.querySelectorAll('.nav-links > li > a');
+links.forEach(function (link) {
+	link.addEventListener('click', handleLinkClick);
+});
+
+function handleLinkClick(e) {
+	// don't need to call e.preventDefault() because the browser will redirect AFTER running the block
+
+	// toggle .active for active link styling
+	toggleActiveClass(this);
+	// toggle checkbox value(checked)
+	toggler.click();
+}
+
+function toggleActiveClass(linkElem) {
+	links.forEach((link) => link.classList.remove('active'));
+	linkElem.classList.add('active');
+}
